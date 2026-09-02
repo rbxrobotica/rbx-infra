cb66dfa12e28cb18dc5cceb5ed554815450b8da4
Leandro Damasio <ldamasio@gmail.com>
2026-09-01 22:53:01 -0300

    feat(dns): publish Kulinaryos test API endpoint

--- infra/terraform/dns/kulinaryos_com.tf
+++ infra/terraform/dns/kulinaryos_com.tf
@@ -58,6 +58,14 @@ resource "powerdns_record" "social_kulinaryos_com_a" {
   records = ["157.230.125.23"]
 }
 
+resource "powerdns_record" "api_test_kulinaryos_com_a" {
+  zone    = powerdns_zone.kulinaryos_com.name
+  name    = "api-test.kulinaryos.com."
+  type    = "A"
+  ttl     = 300
+  records = ["157.230.125.23"]
+}
+
 resource "powerdns_record" "localhost_kulinaryos_com_a" {
   zone    = powerdns_zone.kulinaryos_com.name
   name    = "localhost.kulinaryos.com."
