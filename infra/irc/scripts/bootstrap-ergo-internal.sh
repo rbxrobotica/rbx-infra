294924bf54057ad3a06c65222b9a603aa70f4440
Leandro Damasio <ldamasio@gmail.com>
2026-09-24 01:10:49 -0300

    fix(irc): detach Ergo smoke test from remote stdin

--- infra/irc/scripts/bootstrap-ergo-internal.sh
+++ infra/irc/scripts/bootstrap-ergo-internal.sh
@@ -119,7 +119,7 @@ if [[ ! "$tls_bind" =~ ^127\.0\.0\.1$ && \
     exit 23
 fi
 docker compose --env-file .env -f compose.yaml config --quiet
-docker compose --env-file .env -f compose.yaml run --rm --no-deps \
+docker compose --env-file .env -f compose.yaml run --rm --no-deps -T \
     --entrypoint /ircd-bin/ergo ergo run --conf /ircd/ircd.yaml --smoke
 docker compose --env-file .env -f compose.yaml up -d ergo
 docker compose --env-file .env -f compose.yaml ps ergo
